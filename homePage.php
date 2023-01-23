7a4823e65e6d2ec1bb71b61b3c77846a8b154b53
Mathéo Guilbert <maethik007@gmail.com>
2023-01-23 18:20:57 +0100

    firstCommit

---
+++ homePage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/homePage.css">
+	<title>Home Page</title>
+</head>
+
+<body>
+	<div class="homePage-container">
+		<div class="homePage-top">
+        <nav>
+			<a class="txtHover" href="#">A propos</a>
+			<a class="txtHover" href="#">Mon CV</a>
+			<a class="txtHover" href="#">Mes projets</a>
+			<a class="txtHover" href="#">Me contacter</a>
+        </nav>
+
+        <button type="button" aria-label="toggle cutain navigation" class="nav-toggler">
+            <span class="line l1"></span>
+            <span class="line l2"></span>
+            <span class="line l3"></span>
+        </button>
+
+			<!-- Titre d'accroche -->
+			<div class="textAcroche">
+				<h1>Bonjour et bienvenue sur mon site !</h1>
+			</div>
+		</div>
+		<div class="homePage-bottom">
+
+		</div>
+	</div>
+
+	<script src="js/homePage.js"></script>
+</body>
+
+</html>
---
+++ main.php
@@ -0,0 +1,11 @@
+<DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Bienvenue sur mon PortFolio !</title>
+</head>
+
+<body>
+	²
+</body>
+</html>
