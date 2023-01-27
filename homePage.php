e5d8425496d18084f9cccb3692919c20992f3c79
Maethik <116798690+Maethik@users.noreply.github.com>
2023-01-27 12:45:22 +0100

    v1

--- homePage.php
+++ homePage.php
@@ -16,16 +16,34 @@
 				<a class="txtHover" href="#">Mes projets</a>
 				<a class="txtHover" href="#">Me contacter</a>
 			</nav>
+			<div class="Home-container">
+				<div class="nav-container">
+					<button type="button" aria-label="toggle cutain navigation" class="nav-toggler">
+						<span class="line l1"></span>
+						<span class="line l2"></span>
+						<span class="line l3"></span>
+					</button>
 
-			<button type="button" aria-label="toggle cutain navigation" class="nav-toggler">
-				<span class="line l1"></span>
-				<span class="line l2"></span>
-				<span class="line l3"></span>
-			</button>
+					<div class="titleNav">
+						<p>♠♥</p>
+					</div>
+				</div>
+				<!-- Titre d'accroche -->
+				<div class="textAcroche-container">
+					<div class="textAcroche">
+						<h1>Bonjour et bienvenue sur <em>mon portfolio</em> !</h1>
+					</div>
 
-			<!-- Titre d'accroche -->
-			<div class="textAcroche">
-				<h1>Bonjour et bienvenue sur mon site !</h1>
+					<div class="changeTextBtn-container">
+						<p>Dois-je vous dire :</p>
+						<div class="btn-container">
+							<button type="button" aria-label="change text to bonjour" class="bonjourbtn btn" id="bonjour">Bonjour</button>
+							<span></span>
+							<button type="button" aria-label="change text to bonsoir" class="bonsoirbtn btn" id="bonsoir">Bonsoir</button>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 		<div class="homePage-bottom">
